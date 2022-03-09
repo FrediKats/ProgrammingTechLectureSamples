4f2e9d361a291b6ab7650cfc9bdcdac3dae7f451
Fredi Kats <fredikats@outlook.com>
2022-03-09 12:54:48 +0300

    Add project files.

---
+++ Lecture01/Slide01_SampleForIl.cs
@@ -0,0 +1,19 @@
+﻿namespace Lecture01;
+
+using static System.Console;
+using static System.Linq.Enumerable;
+
+public class Slide01_SampleForIl
+{
+    public static void Show()
+    {
+        int Square(int x) => x * x;
+
+        int SumOfSquares(int n) =>
+            Range(1, n)
+                .Select(Square)
+                .Sum();
+
+        WriteLine(SumOfSquares(100));
+    }
+}
---
+++ Lecture01FSharp/Slide01_SampleForIl.fs
@@ -0,0 +1,10 @@
+﻿module Slide01_SampleForIl
+
+let square x = x * x
+
+let sumOfSquares n =
+   [1..n]
+   |> List.map square
+   |> List.sum
+
+printfn "%d" (sumOfSquares 100)
